4d68fbc242d35eb78344b4726367338ede839c77
Odoo-PSI <odoo@ampd2.odoo.com>
2020-03-06 23:23:18 +0000

    SZM MO Traveler Commit 5

--- szm_motraveler/__manifest__.py
+++ szm_motraveler/__manifest__.py
@@ -25,7 +25,7 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/mrp_production_views.xml',
+        # 'views/mrp_production_views.xml',
         'report/mrp_production_templates.xml',
         'report/mrp_report_views.xml',
     ],
